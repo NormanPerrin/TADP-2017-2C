120dc02ffd2098084781a47b5f8bbe84117d9e6f
Pablo David Scalora <pablo.scalora@gmail.com>
2017-09-26 14:58:31 -0300

    Punto 2 completo TODO: *) Hacer test

--- opcion5.rb
+++ opcion5.rb
@@ -57,121 +57,46 @@ module Persistencia
       attr_reader descripcion[:named]
       crear_setter(tipo_dato, descripcion[:named])
     end
-    def has_many
-      p "soy has_many"
+    def armarObjeto(datosInstancia)
+      obj = self.new
+      datosInstancia.each { |atributo,valor| obj.__send__("#{atributo}=", valor) }
+      obj
     end
-  end
-
-  def self.const_missing(sym)
-    ConstructorClasePersistente.new(sym)
-  end
-
-  def self.clase_persistente(entorno_persitencia, &bloque)
-    if entorno_persitencia.class == ConstructorClasePersistente
-      entorno_persitencia.construir_clase_persistible(&bloque)
-    else
-      entorno_persitencia.class_eval &bloque
+    def all_instances
+      vectorInstancias ||= []
+      instancias = ObjectSpace.each_object(self)
+      TADB::DB.table(self.name.to_s).entries().each  {
+          |entrada|
+        instanciasCoincidentes = instancias.select { |instancia| instancia.id == entrada[:id] }
+        if (instanciasCoincidentes.length == 1)
+          instanciaParaAgregar = instanciasCoincidentes.first
+        else
+          instanciaParaAgregar = armarObjeto(entrada)
+        end
+        vectorInstancias << instanciaParaAgregar
+      }
+      vectorInstancias
     end
-
-  end
-  module MetodosDeInstancia
-    def crear_hash_atributos
-      hash ||= {}
-      self.class.class_variable_get("@@atributosPersistibles").each do
-      |nombreAtributo, tipo|
-        hash[nombreAtributo]=self.send(nombreAtributo)
+    def buscar_instancias_coincidentes(metodo, args)
+      self.all_instances.select do
+      |instancia|
+        args[0]===instancia.__send__(metodo)
       end
-      hash
     end
-
-    def save!
-      if @id.nil?
-        hash = crear_hash_atributos
-        self.class.__send__(:attr_accessor, "id")
-        self.__send__("id=", TADB::DB.table(self.class.name.to_s).insert(hash))
+    def method_missing(symbol, *args, &block)
+      if !(/^find_by_/.match(symbol.to_s).nil?)
+        metodo = symbol.to_s[8..-1].to_sym
+        raise ArgumentError, "El ​mensaje​ #{metodo} ​recibe​ argumentos" unless
+            instance_method(metodo).arity == 0
+        buscar_instancias_coincidentes(metodo, args)
       else
-        refresh!
-      end
-      @id
-    end
-
-    def borrar_registro_si_existe
-      raise TypeError, "​El​ ​objeto​ #{self} no​ ​tiene​ ​id!" unless !@id.nil?
-      TADB::DB.table(self.class.name.to_s).delete(@id)
-    end
-
-    def refresh!
-      borrar_registro_si_existe
-      hash = crear_hash_atributos
-      hash[:id]=@id
-      TADB::DB.table(self.class.name.to_s).insert(hash)
-      @id
-    end
-
-    def forget!
-      borrar_registro_si_existe
-      @id=nil
-    end
-  end
-endmodule Persistencia
-  require 'tadb'
-  class ConstructorClasePersistente
-    attr_accessor :klass, :superklass
-
-    def <(superklass)
-      self.superklass= superklass
-      return self
-    end
-
-    def initialize(constante)
-      @klass=constante
-    end
-
-    def obtener_clase_persistible(builder)
-      ##### Define al @klass dependiendo si tiene superclase, y crea la constante en Object
-      ##### Si es un builder solamente pasara una vez en la declaracion de la clase persistible luego para
-      ##### Open Class lo hara como constante
-      if !(@superklass.nil?)
-        @clase = Class.new(@superklass)
-      else
-        @clase = Class.new
-      end
-      Object.const_set @klass, @clase
-    end
-
-    def construir_metodos
-      @clase.extend(MetodosDeClase)
-      @clase.include(MetodosDeInstancia)
-    end
-
-    def construir_evaluando_bloque(bloque)
-      @clase.class_eval &bloque
-    end
-
-    def construir_clase_persistible(&bloque)
-      obtener_clase_persistible(self)
-      construir_metodos
-      construir_evaluando_bloque(bloque)
-    end
-  end
-
-  module MetodosDeClase
-    def crear_atributo_en_autoclase(tipo, nombre_simbolo)
-      self.class_variable_set(:@@atributosPersistibles, {}) unless
-          self.class_variable_defined?(:@@atributosPersistibles)
-      (self.class_variable_get(:@@atributosPersistibles))[nombre_simbolo]=tipo
-    end
-    def crear_setter(tipo, nombre)
-      define_method(nombre.to_s+"=") do |valor|
-        raise TypeError, "El valor no es #{tipo.to_s}" unless valor.is_a?tipo
-        self.instance_variable_set("@#{nombre}",valor)
+        super(symbol, *args, &block)
       end
     end
-    def has_one(tipo_dato, descripcion = {})
-      crear_atributo_en_autoclase(tipo_dato, descripcion[:named])
-      attr_reader descripcion[:named]
-      crear_setter(tipo_dato, descripcion[:named])
+    def self.respond_to_missing?(symbol, include_all=false)
+      !(/^find_by_/.match(symbol.to_s).nil?)
     end
+
     def has_many
       p "soy has_many"
     end
--- opcion5_spec.rb
+++ opcion5_spec.rb
@@ -2,79 +2,101 @@ require 'rspec'
 require_relative 'opcion5'
 
 using Persistencia
-  context "con una clase que no existe" do
-    it 'deberia dejarme crear las clases' do
-      module Persistencia
-        clase_persistente A do
-          attr_accessor :uno, :dos
-          has_one String, named: :nombre
-          has_one String, named: :numero
-          has_one Numeric, named: :numero
-          attr_accessor :tres, :cuatro
-          attr_reader :cinco
-          def metodoClaseA
-            p "metodoClaseA"
-          end
+context "con una clase que no existe" do
+  it 'deberia dejarme crear las clases' do
+    module Persistencia
+      clase_persistente A do
+        attr_accessor :uno, :dos
+        has_one String, named: :nombre
+        has_one String, named: :numero
+        has_one Numeric, named: :numero
+        attr_accessor :tres, :cuatro
+        attr_reader :cinco
+        def metodoClaseA
+          nombre === "3" && numero===12
         end
-        a= A.new
-        p "Comienzo"
-        p a.methods
-        p A.instance_methods(false)
-        p A.singleton_methods
-        p A.methods
-        p A.ancestors
-        p A.instance_variables
-        p A.class_variable_get(:@@atributosPersistibles)
-        p a.nombre
-        a.nombre="3"
-        p a.nombre
-        p a.numero
-        a.numero=3
-        p a.numero
-        a.save!
-        p a.id
-        a.numero=9
-        a.refresh!
-        p a.numero
-        p a.id
-        a.forget!
-        p a.id
-        p "Fin"
-
-        class B
-          attr_accessor :atributoClaseB
-          def metodoClaseB
-            p "metodoClaseB"
-          end
+        def metodoClaseAConArgs(rr)
+          nombre === "3" && numero===12
         end
-        clase_persistente C < B  do
-          attr_accessor :uno, :dos
-          has_one String, named: :nombre
-          has_one Numeric, named: :numero
-          attr_accessor :tres, :cuatro
-          attr_reader :cinco
-          def metodoClaseC
-            p "metodoClaseC"
-          end
-        end
-        c=C.new
-        p "Comienzo"
-        p c.methods
-        p C.instance_methods(false)
-        p C.singleton_methods
-        p C.methods
-        p C.ancestors
-        p C.instance_variables
-        p c.nombre
-        c.nombre="3"
-        p c.nombre
-        p c.numero
-        c.numero=3
-        p c.numero
-        c.metodoClaseC
-        c.metodoClaseB
-
-        p "Fin"
       end
+      a= A.new
+      otra_a= A.new
+      otra_a.nombre="rrr"
+      otra_a.numero=35
+      otra_a.save!
+      p "Comienzo"
+      p a.methods
+      p A.instance_methods(false)
+      p A.singleton_methods
+      p A.methods
+      p A.ancestors
+      p A.instance_variables
+      p A.class_variable_get(:@@atributosPersistibles)
+      p a.nombre
+      a.nombre="3"
+      p a.nombre
+      p a.numero
+      a.numero=3
+      p a.numero
+      a.save!
+      p a.id
+      a.numero=12
+      a.refresh!
+      p a.numero
+      p a.id
+      i = A.all_instances
+      p i
+      p i.last.numero
+      i = A.find_by_numero(9)
+      p i
+      p i.last.numero
+      i = A.find_by_nombre("rrr")
+      p i
+      p i.last.nombre if !i.last.nil?
+      i = A.find_by_metodoClaseA(true)
+      p i
+      p i.last.nombre if !i.last.nil?
+      i = A.find_by_metodoClaseAConArgs(true)
+      p i
+      p i.last.nombre if !i.last.nil?
+      a.forget!
+      p a.id
+      p "Fin"
+
+      class B
+        attr_accessor :atributoClaseB
+        def metodoClaseB
+          p "metodoClaseB"
+        end
+      end
+      clase_persistente C < B  do
+        attr_accessor :uno, :dos
+        has_one String, named: :nombre
+        has_one Numeric, named: :numero
+        attr_accessor :tres, :cuatro
+        attr_reader :cinco
+        def metodoClaseC
+          p "metodoClaseC"
+        end
+      end
+      c=C.new
+      p "Comienzo"
+      p c.methods
+      p C.instance_methods(false)
+      p C.singleton_methods
+      p C.methods
+      p C.ancestors
+      p C.instance_variables
+      p c.nombre
+      c.nombre="3"
+      p c.nombre
+      p c.numero
+      c.numero=3
+      p c.numero
+      c.metodoClaseC
+      c.metodoClaseB
+
+      p "Fin"
     end
   end
+end

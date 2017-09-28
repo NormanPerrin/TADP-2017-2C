20b19aaf3daef83c35c9eb254034a5e03ffd6857
David Perez <davidperez@est.frba.utn.edu.ar>
2017-09-28 18:38:52 -0300

    refactor de componentes de orm en varios archivos para legibilidad.

---
+++ intelligentdb.rb
@@ -0,0 +1,45 @@
+require 'tadb'
+
+class IntelligentDB
+
+  def initialize(clase)
+    @table = TADB::DB.table(clase)
+  end
+
+  def insertOrUpdate(hash)
+    if self.search_by_id(hash[:id]).nil?
+      return insert(hash)
+    else
+      return update(hash)
+    end
+  end
+
+  def insert(hash)
+    @table.insert(hash)
+  end
+
+  def update(hash)
+    #TODO: que hacemos con un update?
+    @table.insert(hash)
+  end
+
+  def entries
+    @table.entries
+  end
+
+  def search_by_id(id)
+    posible = self.search_by(:id, id)
+    return nil if posible.length == 0
+    return posible[0] if posible.length == 1
+    raise IOError "La base informa #{posible.length} registros con ese id."
+  end
+
+  def search_by(field, value)
+    @table.entries.select {|hash| hash[field]==value}
+  end
+
+  def delete(id)
+    @table.delete(id)
+  end
+
+end
